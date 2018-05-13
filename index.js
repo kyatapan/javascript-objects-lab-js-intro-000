a9e487967e4491dcb9006ad703cda806e1071fa7
kathrin v <go.kathrin@gmail.com>
2018-05-13 23:53:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObject = Object.assign(object, { [key]: value });
-   return newObject;
+  var newObject = Object.assign({}, { [key]: value });
+ //  return newObject;
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
